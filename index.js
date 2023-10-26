4e89401d1f7fed14d1ae5b4b8eb9b6904d6f49cb
Shreya-The-Great <148854878+Shreya-The-Great@users.noreply.github.com>
2023-10-26 19:18:38 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello world!');
+
+let person = {
+    name: 'shreya',
+    age: 19
+}
+console.log(person);
+
+person.age = 20;
+console.log(person.age);
+person['name'] = 'sri';
+console.log(person.name);
+
+let selectedFruits = ['apple','orange',3];
+console.log(selectedFruits);
